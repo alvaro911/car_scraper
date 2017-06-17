c9699fbc6166a21e234a2081fa40316e3ade96b2
alvaro911 <algp232@hotmail.com>
2017-06-16 18:28:15 -0600

    added new tests

--- test/test_car_finder_app.js
+++ test/test_car_finder_app.js
@@ -17,7 +17,7 @@ function mockCarData(){
   for(let i=1; i<=10; i++){
     mockData.push(generateCarData())
   }
-  return mockData
+  return CarList.insertMany(mockData)
 }
 
 function getCar(){
@@ -25,9 +25,16 @@ function getCar(){
   return carModels[Math.floor(Math.random() * carModels.length)]
 }
 
+function getId(){
+  const ids = ['6176553016', '6166894499', '6168207742', '6150005952', '6156060016', '6168521897', '6146714030', '6143860753', '6167413623', '6165254050']
+  for(let i=0; i<ids.length;i++){
+    return ids[i]
+  }
+}
+
 function generateCarData(){
   return {
-    _id: faker.random.uuid(),
+    id: getId(),
     city: faker.address.city(),
     title: faker.lorem.sentence(),
     price: faker.commerce.price(),
@@ -38,24 +45,23 @@ function generateCarData(){
   }
 }
 
-function disconnectDb(){
-  console.log('Disconnect from database')
-  return mongoose.disconnect()
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
 }
 
 describe('Used cars API resource', function(){
-  let mockResults;
 
   before(function(){
     return runServer(TEST_DATABASE_URL)
   })
 
   beforeEach(function(){
-    return mockResults = mockCarData()
+    return mockCarData()
   })
 
   afterEach(function(){
-    return disconnectDb()
+    return tearDownDb()
   })
 
   after(function(){
@@ -78,14 +84,77 @@ describe('Used cars API resource', function(){
         })
     })
 
-    it.only('Should get a list of cars depending on the city', function(){
+    it('Should get a list of cars depending on the city', function(){
       return chai.request(app)
         .get('/cars')
         .query({city: 'denver'})
         .then(function(res){
           res.should.have.status(200)
           res.body.should.be.a('array')
+          res.should.be.json
+          res.body.forEach(function(car){
+            car.should.be.a('object')
+            car.should.include.keys('_id', 'city', 'model', 'title', 'price', 'img', 'paragraph', 'link')
+          })
         })
+    })
+
+    it('Should get a list of cars depending on car make', function(){
+      return chai.request(app)
+        .get('/cars')
+        .query({model: 'toyota'})
+        .then(function(res){
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.should.be.json
+          res.body.forEach(function(car){
+            car.should.be.a('object')
+            car.should.include.keys('_id', 'city', 'model', 'title', 'price', 'img', 'paragraph', 'link')
+          })
+        })
+    })
+
+    it('Should get a list of cars depending on the city and car make', function(){
+      return chai.request(app)
+        .get('/cars')
+        .query({model: 'toyota', city: 'denver'})
+        .then(function(res){
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          res.should.be.json
+          res.body.forEach(function(car){
+            car.should.be.a('object')
+            car.should.include.keys('_id', 'city', 'model', 'title', 'price', 'img', 'paragraph', 'link')
+          })
+        })
+    })
+  })
+
+  describe('Get cars/:id endpoint', function(){
+    it('Should return a car depending on its Id', function(){
+      let car, res
+      return CarList.findOne()
+        .exec()
+        .then(function(_car){
+          car = _car
+          return chai.request(app)
+            .get(`/car/${car.id}`)
+            console.log('==================')
+            console.log(car.id)
+            .then(function(_res){
+              res = _res
+              res.should.have.status(200)
+              res.body.carlists.should.be.a('object')
+              res.body.carlists.id.should.equal(car.id)
+              res.body.carlists.city.should.equal(car.city)
+              res.body.carlists.model.should.equal(car.model)
+              res.body.carlists.title.should.equal(car.title)
+              res.body.carlists.price.should.equal(car.price)
+              res.body.carlists.img.should.equal(car.img)
+              res.body.carlists.paragraph.should.equal(car.paragraph)
+              res.body.carlists.link.should.equal(car.link)
+            })
+      })
     })
   })
 })
